7a306a5fe8256f2da5d69b47d9abaca738005771
Vladimirs Nordholm <v@vladde.net>
2020-08-23 22:42:57 +0200

    remove debug code

--- js/main.js
+++ js/main.js
@@ -31,30 +31,3 @@ function optimizeLevelCode(levelCode) {
 }
 
 document.addEventListener('DOMContentLoaded', init);
-
-//console.log(Level.fromLevelCode('A916P0A1P0A1C0J1G0J1C0A1C0J1C0A1C0J1C0A1C0D9F1C0A1P0A1P0Q').toLevelCode());
-//console.log(Level.fromLevelCode('A906ZZ1A07A6ZZZF9ZE').toLevelCode());
-
-/*
-let s = ""
-tiles.forEach(function (tile, index) {
-
-    if (index && index % 16 === 0) {
-
-        // just to make sure firefox doesn't group console logs together
-        for (let i = 0; i < index; i++) {
-            s += " "
-        }
-
-        console.log(s)
-        s = "";
-    }
-
-    s += tile.shape
-    //s += " "
-
-})
-if (s !== "") {
-    console.log(s)
-}
-*/
