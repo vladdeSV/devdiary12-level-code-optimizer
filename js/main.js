a7791b55a538975d64a02d73c73f63aa6f0fc029
Vladimirs Nordholm <v@vladde.net>
2020-08-23 22:19:49 +0200

    add nice page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,39 @@
 import {Level} from "./level.js";
 
+function init() {
+    let button = document.querySelector('#js-button-optimize');
+    let input = document.querySelector('#js-level-code')
+    let output = document.querySelector('#js-level-code-output')
 
-console.log(Level.fromLevelCode('A916P0A1P0A1C0J1G0J1C0A1C0J1C0A1C0J1C0A1C0D9F1C0A1P0A1P0Q').toLevelCode());
-console.log(Level.fromLevelCode('A906ZZ1A07A6ZZZF9ZE').toLevelCode());
+    input.focus();
+
+    button.addEventListener('click', () => {
+        output.value = optimizeLevelCode(input.value)
+    });
+}
+
+function optimizeLevelCode(levelCode) {
+    levelCode = levelCode.trim()
+
+    if (!RegExp('^[A-Z]+9[A-Zsn0-9]*$').test(levelCode)) {
+        return 'invalid level code';
+    }
+
+    let optimizedLevelCode;
+    try {
+        let level = Level.fromLevelCode(levelCode);
+        optimizedLevelCode = level.toLevelCode();
+    } catch (error) {
+        optimizedLevelCode = 'unknown error encountered'
+    }
+
+    return optimizedLevelCode;
+}
+
+document.addEventListener('DOMContentLoaded', init);
+
+//console.log(Level.fromLevelCode('A916P0A1P0A1C0J1G0J1C0A1C0J1C0A1C0J1C0A1C0D9F1C0A1P0A1P0Q').toLevelCode());
+//console.log(Level.fromLevelCode('A906ZZ1A07A6ZZZF9ZE').toLevelCode());
 
 /*
 let s = ""
